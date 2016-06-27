24a37a8e3731aea4c2ff02d4da90bc2312a51c67
Patrick <psperkins@gmail.com>
2016-06-27 12:16:00 -0600

    adding v 1.4.1 from https://plugins.svn.wordpress.org/bwp-google-xml-sitemaps/tags/1.4.1/

--- bwp-simple-gxs.php
+++ bwp-simple-gxs.php
@@ -2,19 +2,21 @@
 /*
 Plugin Name: Better WordPress Google XML Sitemaps
 Plugin URI: http://betterwp.net/wordpress-plugins/google-xml-sitemaps/
-Description: A more lightweight Google XML Sitemap WordPress plugin that generates a <a href="http://en.wikipedia.org/wiki/Sitemap_index">Sitemap index</a> rather than a single sitemap. Despite its simplicity, it is still very powerful and has plenty of options to choose.
-Version: 1.3.1
-Text Domain: bwp-simple-gxs
+Description: Generates XML sitemaps for your WordPress website with ease. This plugin comes with support for sitemap index, multisite WordPress and Google News sitemap. It also provides a powerful and flexible system for any customization need.
+Version: 1.4.1
+Text Domain: bwp-google-xml-sitemaps
 Domain Path: /languages/
 Author: Khang Minh
 Author URI: http://betterwp.net
 License: GPLv3 or later
 */
 
-// In case someone integrates this plugin in a theme or calling this directly
-if (class_exists('BWP_SIMPLE_GXS') || !defined('ABSPATH'))
-	return;
-
-// init plugin
-require_once dirname(__FILE__) . '/includes/class-bwp-simple-gxs.php';
-$bwp_gxs = new BWP_SIMPLE_GXS();
+/**
+ * The current file (`bwp-simple-gxs.php`) is used to make sure updates to
+ * 1.4.0 is smooth.
+ *
+ * There's no easy way to change a plugin's main file, need to find a solution.
+ * For now we load the actual main plugin file via a simple `include_once`
+ * statement.
+ */
+include_once dirname(__FILE__) . '/bwp-gxs.php';
